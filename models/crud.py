23a966d2a34f7b6f1418126bed91524f0edf6eb4
tejas <tejaskulkarni98902@gmail.com>
2016-09-17 18:45:18 +0530

    Database Signup made and few changes

--- models/crud.py
+++ models/crud.py
@@ -3,4 +3,7 @@ from connect import db
 users = db.users
 
 def insert(document):
-	users.insert_one(document)
+	return users.insert_one(document)
+
+def find(document):
+	return users.find(document)
